5fff165713834d48ebdabcaa9d379f85fbd5a24b
Subhadip Ghosh <subhadip.sky@gmail.com>
2016-02-24 23:34:57 +0530

    License Information added.

--- src/main/java/com/subhadip/phonebookprocessor/reader/VcardPhonebookReaderImpl.java
+++ src/main/java/com/subhadip/phonebookprocessor/reader/VcardPhonebookReaderImpl.java
@@ -16,31 +16,6 @@
  *******************************************************************************/
 package com.subhadip.phonebookprocessor.reader;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import com.subhadip.phonebookprocessor.logger.TestLogger;
-
-public class PhonebookReaderImplTest {
-
-	private PhonebookReader phonebookReader = new CsvPhonebookReaderImpl(); 
-	
-	@BeforeClass
-	public static void setUp() throws Exception{
-		TestLogger.setUp();
-	}
-	
-	@Test
-	public void testReadPhonebook() {
-		Set<String> manadatoryParams = new HashSet<>();
-		manadatoryParams.add("Name");
-		Collection<Map<String, String>> resultMaps = phonebookReader.readPhonebook("src/test/data/googleContacts.csv", "utf-16", ",", manadatoryParams);
-		System.out.printf("Returned collection size: %d", resultMaps.size());
-	}
+public class VcardPhonebookReaderImpl {
 
 }
